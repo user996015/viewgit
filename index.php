2be6cbe6b1c535b8098b96cb865353efe404cf7a
Heikki Hokkanen <hoxu@users.sf.net>
2008-05-01 17:46:24 +0300

    Added basic header/footer templates and empty index.

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
 
+error_reporting(E_ALL);
+
+$action = 'index';
+$template = '';
+$page['title'] = 'ViewGit';
+
+if ($action === 'index') {
+	$template = 'index';
+}
+else {
+	die('Invalid action');
+}
+
+require 'templates/header.php';
+require "templates/$template.php";
+require 'templates/footer.php';
