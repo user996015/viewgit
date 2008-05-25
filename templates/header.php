c5f5a67f7711cfc6864aa9d15655636ac364a122
Heikki Hokkanen <hoxu@users.sf.net>
2008-05-25 15:30:57 +0300

    Don't escape html/title, assume it already is.

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title><?php echo htmlentities($page['title']); ?></title>
+	<title><?php echo $page['title']; ?></title>
 	<link rel="stylesheet" href="default.css" type="text/css" />
 </head>
 <body>
