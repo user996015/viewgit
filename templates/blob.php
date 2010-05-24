907e1c9a04a0485c267ebb241ecd02872dfdfaf1
Heikki Hokkanen <hoxu@users.sf.net>
2010-05-24 18:58:59 +0300

    Refactored how last commit info is shown for blobs.
    Style is similar to the commitdiff page now.

--- templates/blob.php
+++ templates/blob.php
@@ -1,7 +1,16 @@
-<h1>Last commit</h1>
+<h2>Last commit for <?php echo $page['path']; ?>: <a href="<?php echo makelink(array('a' => 'commit', 'p' => $page['project'], 'h' => $page['lastlog']['h'])); ?>"><?php echo htmlentities_wrapper($page['lastlog']['h']); ?></a></h2>
+<h1><?php echo htmlentities_wrapper($page['lastlog']['message_firstline']); ?></h1>
+<div class="authorinfo">
 <?php
-echo $page['lastlog'];
+echo format_author($page['lastlog']['author_name']);
+echo ' ['. $page['lastlog']['author_datetime'] .']';
 ?>
+</div>
+<div class="commitmessage">
+<pre>
+<?php echo htmlentities_wrapper($page['lastlog']['message_full']); ?>
+</pre>
+</div>
 <div class="file">
 <?php
 if (isset($page['html_data'])) {
