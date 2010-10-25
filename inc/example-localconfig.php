61ff1c3a190cc3470e352479c3d6de91e28d43e6
Mike Bloy <mike@bloy.org>
2010-10-24 22:56:10 -0500

    Example comment for building conf['projects'] with a small script.
    The example is for gitolite's projects.list.
    
    [hoxu@users.sf.net: slightly modified patch & message]
    
    Signed-off-by: Heikki Hokkanen <hoxu@users.sf.net>

--- inc/example-localconfig.php
+++ inc/example-localconfig.php
@@ -12,6 +12,19 @@ $conf['projects'] = array(
 	),
 );
 
+// $conf['projects'] can also be built up programmatically
+// for example the following can be used to translate gitolite's
+// projects.list (intended for gitweb) for viewgit
+
+/*
+$projects_list_file = "/opt/git/projects.list";
+$repo_home = "/opt/git/repositories";
+foreach (file($projects_list_file) as $proj) {
+	$proj = trim($proj);
+	$conf['projects'][$proj] = array('repo'  => "$repo_home/$proj");
+}
+*/
+
 // A bit sorter datetime format: YY-MM-DD HH:MM
 $conf['datetime'] = '%y-%m-%d %H:%M';
 
