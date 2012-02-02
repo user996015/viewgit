2a43e6905b462656ca965da0861173834df7eb06
user996015 <user996015@github.com>
2012-02-02 10:24:48 -0800

    Added tbody td classes for css targeting.

--- templates/shortlog.php
+++ templates/shortlog.php
@@ -15,9 +15,10 @@ $page['lasthash'] = 'HEAD';
 foreach ($page['shortlog'] as $l) {
 	$tr_class = $tr_class=="odd" ? "even" : "odd";
 	echo "<tr class=\"$tr_class\">\n";
-	echo "\t<td>$l[date]</td>\n";
-	echo "\t<td>". format_author($l['author']) ."</td>\n";
-	echo "\t<td><a href=\"". makelink(array('a' => 'commit', 'p' => $page['project'], 'h' => $l['commit_id'])) ."\">". htmlentities_wrapper($l['message']) ."</a>";
+	echo "\t" . '<td class="date">' . $l[date] . '</td>' . "\n";
+	echo "\t" . '<td class="author">' . format_author($l['author']) ."</td>\n";
+	echo "\t" . '<td class="message">' . '<a href="' . makelink(array('a' => 'commit', 'p' => $page['project'], 'h' => $l['commit_id'])) ."\">". htmlentities_wrapper($l['message']) ."</a>";
+
 	if (count($l['refs']) > 0) {
 		foreach ($l['refs'] as $ref) {
 			$parts = explode('/', $ref);
@@ -29,7 +30,7 @@ foreach ($page['shortlog'] as $l) {
 		}
 	}
 	echo "</td>\n";
-	echo "\t<td>";
+	echo "\t" . '<td class="actions">';
 	echo "<a href=\"". makelink(array('a' => 'commitdiff', 'p' => $page['project'], 'h' => $l['commit_id'])) ."\" class=\"cdiff_link\" title=\"Commit Diff\">commitdiff</a>";
 	echo " <a href=\"". makelink(array('a' => 'tree', 'p' => $page['project'], 'h' => $l['tree'], 'hb' => $l['commit_id'])) ."\" class=\"tree_link\" title=\"Tree\">tree</a>";
 	echo " <a href=\"". makelink(array('a' => 'archive', 'p' => $page['project'], 'h' => $l['tree'], 'hb' => $l['commit_id'], 't' => 'targz')) ."\" rel=\"nofollow\" class=\"tar_link\" title=\"tar/gz\">tar/gz</a>";
