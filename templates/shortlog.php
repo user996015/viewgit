a371ebd11de08bede540fc7fe3855b4b870d79e4
Zach Borboa <zach.borboa@opensgi.com>
2012-02-24 11:16:47 -0800

    Clean up template

--- templates/shortlog.php
+++ templates/shortlog.php
@@ -1,50 +1,63 @@
-<h1><a href="<?php echo makelink(array('a' => 'shortlog', 'p' => $page['project'])); ?>">Shortlog</a></h1>
+<h1>
+    <a href="<?php echo makelink(array('a' => 'shortlog', 'p' => $page['project'])); ?>">Shortlog</a>
+</h1>
 
 <table class="shortlog">
-<thead>
-<tr>
-	<th class="date">Date</th>
-	<th class="author">Author</th>
-	<th class="message">Message</th>
-	<th class="actions">Actions</th>
-</tr>
-</thead>
-<tbody>
+    <thead>
+        <tr>
+            <th class="date">Date</th>
+            <th class="author">Author</th>
+            <th class="message">Message</th>
+            <th class="actions">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
 <?php
 $page['lasthash'] = 'HEAD';
+
 foreach ($page['shortlog'] as $l) {
-	$tr_class = $tr_class=="odd" ? "even" : "odd";
-	echo '<tr class="' . $tr_class . '">' . "\n";
-	echo "\t" . '<td class="date">' . $l['date'] . '</td>' . "\n";
-	echo "\t" . '<td class="author">' . format_author($l['author']) ."</td>\n";
-	echo "\t" . '<td class="message">' .
-        '<a href="' . makelink(array('a' => 'commit', 'p' => $page['project'], 'h' => $l['commit_id'])) . '">' .
-            htmlentities_wrapper($l['message']) .
-        '</a>';
+    $tr_class = $tr_class=='odd' ? 'even' : 'odd';
 
-	if (count($l['refs']) > 0) {
-		foreach ($l['refs'] as $ref) {
-			$parts = explode('/', $ref);
-			$shortref = join('/', array_slice($parts, 1));
-			$type = 'head';
-			if ($parts[0] == 'tags') { $type = 'tag'; }
-			elseif ($parts[0] == 'remotes') { $type = 'remote'; }
-			echo "<span class=\"label $type\" title=\"$ref\">$shortref</span>";
-		}
-	}
+    echo
+        '<tr class="' . $tr_class . '">' .
+            '<td class="date">' . $l['date'] . '</td>' .
+            '<td class="author">' . format_author($l['author']) . '</td>' .
+            '<td class="message">' .
+                '<a href="' . makelink(array('a' => 'commit', 'p' => $page['project'], 'h' => $l['commit_id'])) . '">' .
+                    htmlentities_wrapper($l['message']) .
+                '</a>';
 
-	echo '</td>' . "\n";
+    if (count($l['refs']) > 0) {
+        foreach ($l['refs'] as $ref) {
+            $parts = explode('/', $ref);
+            $shortref = join('/', array_slice($parts, 1));
+            $type = 'head';
 
-	echo "\t" . '<td class="actions">';
-	echo "<a href=\"". makelink(array('a' => 'commitdiff', 'p' => $page['project'], 'h' => $l['commit_id'])) ."\" class=\"cdiff_link\" title=\"Commit Diff\">commitdiff</a>";
-	echo " <a href=\"". makelink(array('a' => 'tree', 'p' => $page['project'], 'h' => $l['tree'], 'hb' => $l['commit_id'])) ."\" class=\"tree_link\" title=\"Tree\">tree</a>";
-	echo " <a href=\"". makelink(array('a' => 'archive', 'p' => $page['project'], 'h' => $l['tree'], 'hb' => $l['commit_id'], 't' => 'targz')) ."\" rel=\"nofollow\" class=\"tar_link\" title=\"tar/gz\">tar/gz</a>";
-	echo " <a href=\"". makelink(array('a' => 'archive', 'p' => $page['project'], 'h' => $l['tree'], 'hb' => $l['commit_id'], 't' => 'zip')) ."\" rel=\"nofollow\" class=\"zip_link\" title=\"zip\">zip</a>";
-	echo " <a href=\"". makelink(array('a' => 'patch', 'p' => $page['project'], 'h' => $l['commit_id'])) ."\" class=\"patch_link\" title=\"Patch\">patch</a>";
-	echo "</td>\n";
+            if ($parts[0] == 'tags') {
+                $type = 'tag';
+            }
+            elseif ($parts[0] == 'remotes') {
+                $type = 'remote';
+            }
 
-	echo '</tr>' . "\n";
-	$page['lasthash'] = $l['commit_id'];
+            echo '<span class="label ' . $type . '" title="' . $ref . '">' . $shortref . '</span>';
+        }
+    }
+
+    echo
+        '</td>' .
+
+        '<td class="actions">' .
+            '<a href="' . makelink(array('a' => 'commitdiff', 'p' => $page['project'], 'h' => $l['commit_id'])) . '" class="cdiff_link" title="Commit Diff">commitdiff</a>' .
+            '<a href="' . makelink(array('a' => 'tree', 'p' => $page['project'], 'h' => $l['tree'], 'hb' => $l['commit_id'])) . '" class="tree_link" title="Tree">tree</a>' .
+            '<a href="' . makelink(array('a' => 'archive', 'p' => $page['project'], 'h' => $l['tree'], 'hb' => $l['commit_id'], 't' => 'targz')) . '" rel="nofollow" class="tar_link" title="tar/gz">tar/gz</a>' .
+            '<a href="' . makelink(array('a' => 'archive', 'p' => $page['project'], 'h' => $l['tree'], 'hb' => $l['commit_id'], 't' => 'zip')) . '" rel="nofollow" class="zip_link" title="zip">zip</a>' .
+            '<a href="' . makelink(array('a' => 'patch', 'p' => $page['project'], 'h' => $l['commit_id'])) . '" class="patch_link" title="Patch">patch</a>' .
+        '</td>' .
+    '</tr>' .
+    '';
+
+    $page['lasthash'] = $l['commit_id'];
 }
 ?>
 </tbody>
