e90ce729c3acd5d90789afeefa7c6a7f824563ec
user996015 <user996015@github.com>
2012-03-02 12:29:32 -0800

    Add powered by

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,16 @@
 <?php
 if ($conf['ad']) {
-	echo "<div class=\"ad\"><a href=\"http://viewgit.fealdia.org/\" title=\"Visit the ViewGit homepage\">ViewGit</a></div>";
+    echo "<div class=\"ad\"><a href=\"http://viewgit.fealdia.org/\" title=\"Visit the ViewGit homepage\">ViewGit</a></div>";
 }
+
 VGPlugin::call_hooks('footer');
-?>
-</div>
-</body>
-</html>
+
+echo
+                '<div class="powered-by">' .
+                    'Powered by <a href="http://viewgit.fealdia.org/">ViewGit</a> and<br />' .
+                    'the <a href="https://github.com/user996015/viewgit/">ViewGit Enhancement Suite</a>.' .
+                '</div>' .
+            '</div>' .
+        '</body>' .
+    '</html>' .
+    '';
